6eb101f646f7e0db22e743276fd8cc9fc77b0f0d
KygenHack <148578932+KygenHack@users.noreply.github.com>
2023-11-15 16:29:06 +0100

    Update routes.tsx

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 
 // Admin Imports
-import MainDashboard from "./views/admin/default";
-import NFTMarketplace from "./views/admin/marketplace";
-import Profile from "./views/admin/profile";
-import DataTables from "./views/admin/tables";
+import MainDashboard from "./views/user/default";
+import NFTMarketplace from "./views/user/marketplace";
+import Profile from "./views/user/profile";
+import DataTables from "./views/user/tables";
 import RTLDefault from "./views/rtl/default";
 
 // Auth Imports
@@ -18,21 +18,21 @@ import {
   MdPerson,
   MdLock,
 } from "react-icons/md";
-import StakeNFTpage from "./views/admin/stake";
-import StakeTokenpage from "./views/admin/stakeToken";
-import WalletRecovery from "./views/admin/recovery";
+import StakeNFTpage from "./views/user/stake";
+import StakeTokenpage from "./views/user/stakeToken";
+import WalletRecovery from "./views/user/recovery";
 
 const routes = [
   {
     name: "Smart Portfolio",
-    layout: "/admin",
+    layout: "/user",
     path: "smartportal",
     icon: <MdHome className="h-6 w-6" />,
     component: <MainDashboard />,
   },
   {
     name: "Claim NFTs",
-    layout: "/admin",
+    layout: "/user",
     path: "minting-nfts",
     icon: <MdOutlineShoppingCart className="h-6 w-6" />,
     component: <NFTMarketplace />,
@@ -40,21 +40,21 @@ const routes = [
   },
   {
     name: "Stake NFTs",
-    layout: "/admin",
+    layout: "/user",
     icon: <MdBarChart className="h-6 w-6" />,
     path: "data-tables",
     component: <StakeNFTpage />,
   },
   {
     name: "Liquidity Stake ",
-    layout: "/admin",
+    layout: "/user",
     path: "liquidity-stake",
     icon: <MdPerson className="h-6 w-6" />,
     component: <StakeTokenpage />,
   },
   {
     name: "Wallet Recovery ",
-    layout: "/admin",
+    layout: "/user",
     path: "wallet-recovery",
     icon: <MdBarChart className="h-6 w-6" />,
     component: <WalletRecovery />,
